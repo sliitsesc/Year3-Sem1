97b487f6b08abb9e1f2d4a8e6e66e8a5d1e812f4
DInuwan97 <kalubowila.dc25132@live.com>
2021-02-05 22:47:53 +0530

    Add REST services with Jercy

---
+++ DS/Labs/REST services with Jercy/RESTful_Jersey_Hello_World/WebContent/hello.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    $.ajax({
+    	 xhrFields: {
+    	        withCredentials: true
+    	    },
+    	    beforeSend: function (xhr) {
+    	        xhr.setRequestHeader('Authorization', 'Basic ' + btoa('admin:password123'));
+    	    },
+        url: "http://localhost:8081/RESTful_Jersey_Hello_World/rest/helloworld"
+    }).then(function(data, status, jqxhr) {
+       $('.greeting-id').append(data.id);
+       $('.greeting-content').append(data.content);
+       console.log(jqxhr);
+    });
+});
